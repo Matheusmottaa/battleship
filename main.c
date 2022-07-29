3720b70cce09d3fb938d936440d5fec2aeda9c8e
Matheus <matheusfelipe1918@gmail.com>
2022-07-29 18:33:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,138 +2,142 @@
 #include <stdlib.h> 
 #include <time.h> 
 
-void drawGrid(char ship[][10]); 
-void newSeed(); 
-unsigned int mapColumn(char column); 
-
+void naval(char b[][10]);
+void new_seed(); 
+unsigned int map_col(char col); 
+void grid(char b[][10]); 
 
 int main(){ 
-
-  char ships[8][10] = {0}; 
-
-  char opt  = 'p',
+  char boats[8][10] = {0}, 
+       opt = 'j', 
        sort = 1, 
-       col  = 0; 
-
+       col = 0; 
   unsigned int num, 
-               row      = 0, 
-               hits     = 0, 
-               mistakes = 0; 
+               row = 0, 
+               acertos = 0, 
+               errors = 0; 
   
-  register int i, j; 
+  register int i,j; 
 
   do{ 
+
     if(sort){ 
       system("clear"); 
-      newSeed(); 
-
-      for(i=0; i<8; i++){ 
-        for(j=0; j<10; j++){ 
+      new_seed(); 
+      for(i=0;i<8;i++){ 
+        for(j=0;j<10;j++){ 
           num = rand()%100+1; 
-
-          if(num%2)
-            ships[i][j] = '@'; 
-          else 
-            ships[i][j] = 0x20; 
+          if(num%2) boats[i][j] = '@'; 
+          else      boats[i][j] = 0x20; 
         }
       }
-      drawGrid(ships); 
+      naval(boats); 
     } else { 
-      printf("Invalid option\n"); 
+      printf("Opcao invalida\n"); 
+      system("clear"); 
       sort = 1; 
-    }
-
-    printf("(d) Draft again \n"); 
-    printf("(p) play the game\n"); 
+  }
+    printf("(s) sortear novamente\n"); 
+    printf("(j) jogar!\n"); 
     scanf(" %c", &opt); 
 
-    if(opt != 'd' && opt != 'D') sort=0;  
+  if(opt!='s' && opt!='S') sort = 0; 
 
-  } while(opt != 'p' && opt != 'P'); 
+  }while(opt!='j' && opt!='J'); 
 
   system("clear"); 
-  drawGrid(ships); 
-  printf("Targets hits: %3d | Mistakes: %3d\n", hits, mistakes); 
+  grid(boats);  
+  printf("Alvos atingidos: %3d | Erros: %3d\n", acertos, errors);
 
   while(1){ 
-    printf("Type the trigger position: ");
+    printf("Digite a posicao de disparo: "); 
     scanf(" %c%d", &col, &row); 
-
     system("clear"); 
 
-    if(ships[row-1][mapColumn(col)] == '@'){ 
-      ships[row-1][mapColumn(col)] = '*';
-      hits++; 
-      putchar('\a');  
-    } else { 
-      mistakes++; 
+    if(boats[row-1][map_col(col)] == '@'){ 
+      boats[row-1][map_col(col)] = '*'; 
+      acertos++; 
+      putchar('\a'); 
+    }else{ 
+      errors++; 
       putchar('\a'); 
       putchar('\a'); 
       putchar('\a'); 
-        if(mistakes > 5){ 
-          printf("You lost the game :(\n"); 
-          for(i=0; i<5; i++){ 
-            putchar('\a'); 
-          }
-          system("pause"); 
-          exit(0); 
-        }
-    }
 
-    drawGrid(ships); 
-    printf("Targets hits: %3d | Mistakes: %3d\n", hits, mistakes); 
+      if(errors > 5){ 
+        printf("Fomos Derrotados!\n");
+        for(i=0;i<5;i++){ 
+          putchar('\a'); 
+        }
+        exit(0); 
+      }
+    }
+    grid(boats); 
+    printf("Alvos Atigidos: %3d | Erros: %3d\n", acertos, errors); 
   }
 
-  system("pause"); 
   return 0; 
 }
 
-
-void drawGrid(char ship[][10]){ 
-  register int i, j; 
-
-  printf("  A  B  C  D  E  F  G  H  I  J \n"); 
-  puts("-----------------------------------"); 
-
-  for(i=0; i<8; i++){ 
+void naval(char b[][10]){ 
+  register int i,j; 
+  printf("   A   B   C   D   E   F   G   H   I   J\n"); 
+  puts("--------------------------------------------\n");
+  for(i=0;i<8;i++){ 
     printf("%d", i+1); 
-
-    for(j=0; j<10; j++){ 
-      printf("| %c", ship[i][j]); 
+    for(j=0;j<10;j++){ 
+      printf("| %c ", b[i][j]); 
     }
     puts("|"); 
-    puts("----------------------------------"); 
+  puts("--------------------------------------------\n");
   }
-
 }
 
-void newSeed(){ 
-  unsigned long int capture;
+void new_seed(){ 
+  time_t capture; 
   time(&capture); 
-  srand((unsigned)capture);  
+  srand((unsigned)capture); 
 }
 
-unsigned int mapColumn(char column){
-    switch(column){ 
-      case 'a': 
-      case 'A': return 0; break; 
-      case 'b': 
-      case 'B': return 1; break; 
-      case 'c': 
-      case 'C': return 2; break; 
-      case 'd': 
-      case 'D': return 3; break; 
-      case 'e': 
-      case 'E': return 4; break; 
-      case 'f': 
-      case 'F': return 5; break; 
-      case 'g': 
-      case 'G': return 6; break; 
-      case 'h': 
-      case 'H': return 7; break; 
-      case 'i': 
-      case 'I': return 8; break; 
-      case 'j': 
-      case 'J': return 9; break; 
+unsigned int map_col(char col){ 
+  switch(col){ 
+    case 'a':
+    case 'A': return 0; break;  
+    case 'b':
+    case 'B': return 1; break;     
+    case 'c':
+    case 'C': return 2; break; 
+    case 'd':
+    case 'D': return 3; break; 
+    case 'e':
+    case 'E': return 4; break; 
+    case 'f':
+    case 'F': return 5; break;  
+    case 'g':
+    case 'G': return 6; break; 
+    case 'h':
+    case 'H': return 7; break; 
+    case 'i':
+    case 'I': return 8; break; 
+    case 'j':
+    case 'J': return 9; break;    
+  }
+}
+
+void grid(char b[][10]){ 
+  register int i,j; 
+  printf("  A  B  C  D  E  F  G  H  I  J\n"); 
+  puts("--------------------------------\n");
+  for(i=0;i<8;i++){
+    printf("%d", i+1); 
+    for(j=0;j<10;j++){
+      if(b[i][j] == '*'){ 
+        printf("| %c ", b[i][j]); 
+      }else { 
+        printf("|  "); 
+      }
+    }
+    puts("|"); 
+    puts("--------------------------------\n");
   }
 }
